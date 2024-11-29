9deab4852d2f99974f23bcd3d785f6ad5a53d284
semionc <x.sammythagi@gmail.com>
2024-11-29 23:20:18 +0700

    create new branch drink and update drink detail

--- user_auth/forms.py
+++ user_auth/forms.py
@@ -4,16 +4,11 @@ from user_auth.models import UserProfile
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm) :
-    user_type = forms.ChoiceField(
-        choices = (("admin", "Admin"), ("user", "User")),
-        label="Choose your user type:",
-        required=True
-    )
     email = forms.EmailField(label = "Email", required=True)
 
     class Meta:
         model = User
-        fields = ['user_type','username','email','password1','password2'] 
+        fields = ['username','email','password1','password2'] 
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
@@ -21,6 +16,6 @@ class RegisterForm(UserCreationForm) :
             user.save()
         UserProfile.objects.create(
             user=user,
-            user_type = self.cleaned_data["user_type"],
+            user_type = "user",
         )
         return user
